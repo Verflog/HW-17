0cc05babec3b8439847509a129dd03597552ea79
Verflog <pnbaranova@gmail.com>
2025-03-12 04:34:36 +0300

    Под комментариями в CalculatorController.divide и в MyErrorController вынесены мои попытки вывести отдельную ошибку конкретно для случая num2=0 в divide, но так как спринг на все выкидывает один путь /error так и не разобралась как это сделать и можно ли вообще, да и дз сильно просрочила так что бросила эту затею

--- src/main/java/com/calculator/CalculatorService.java
+++ src/main/java/com/calculator/CalculatorService.java
@@ -1,4 +1,27 @@
 package com.calculator;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CalculatorService {
+
+    public String hello(String message) {
+        return message;
+    }
+
+    public Integer plus(Integer num1, Integer num2){
+        return num1 + num2;
+    }
+
+    public Integer minus(Integer num1, Integer num2){
+        return num1 - num2;
+    }
+
+    public Integer multiply(Integer num1, Integer num2){
+        return num1 * num2;
+    }
+
+    public Integer divide(Integer num1, Integer num2){
+        return num1 / num2;
+    }
 }
